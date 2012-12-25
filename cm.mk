e93f30d4762d3748f85a92b667ff6f9d97793659
Stelios Panayi <stilianos1000@gmail.com>
2012-12-25 20:46:36 +0200

    Initial Commit

---
+++ cm.mk
@@ -0,0 +1,24 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/semc/robyn/device.mk)
+
+# Setup device configuration
+PRODUCT_RELEASE_NAME := E10i
+PRODUCT_DEVICE := robyn
+PRODUCT_NAME := cm_robyn
+PRODUCT_BRAND := SEMC
+PRODUCT_MODEL := E10i
+PRODUCT_MANUFACTURER := Sony Ericsson
+
+# Release data
+PRODUCT_VERSION_DEVICE_SPECIFIC := Alpha 1
+TARGET_NO_LIVEWALLPAPERS := false
+TARGET_BOOTANIMATION_NAME := vertical-240x320
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=E10 BUILD_FINGERPRINT="google/yakju/maguro:4.2.1/JOP40D/533553:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.2.1 JOP40D 533553 release-keys"
