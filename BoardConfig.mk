41934d51e91319b42fbf211e6f512270f782c775
AndroidMeda <hephappy@gmail.com>
2013-03-25 15:44:36 +0200

    Build kernel from source (2/2)
    Change-Id: Ifd587904c49a241424758c7e7feee35560106d82

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,7 @@
 -include device/semc/msm7x27-common/BoardConfigCommon.mk
 
 # Info
+TARGET_KERNEL_CONFIG := cyanogenmod_robyn_defconfig
 TARGET_BOOTLOADER_BOARD_NAME := robyn
 TARGET_OTA_ASSERT_DEVICE := E10i,E10a,robyn
 PRODUCT_BUILD_PROP_OVERRIDES += TARGET_BOOTLOADER_BOARD_NAME=robyn
